d8ee5ce2078f21efe243e12da1a156c50cd9fdd8
Michael Petrochuk <petrochukm@gmail.com>
2016-04-30 12:57:30 -0700

    Inverse map added

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
-var names = require('./');
+var names = require('./index.min.js');
 var assert = require('assert');
 
 assert.deepEqual(names["Black"], "000000");
+assert.deepEqual(names["000000"], "Black");
 assert.deepEqual(names["Alice Blue"], "F0F8FF");
+assert.deepEqual(names["F0F8FF"], "Alice Blue");
